1df9a7006e86652833672eb53fa73bbe40dc02aa
Celestial Phineas <hanghang0713@hotmail.com>
2018-06-24 15:17:01 +0800

    Small modification

--- src/components/public/Welcome.vue
+++ src/components/public/Welcome.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <p>This is the user panel!</p>
-    <p style="text-align:left"><b>User_name:</b> {{user_name}}</p>
-    <p style="text-align:left"><b>User user_id:</b> {{userId}}</p>
-    <p style="text-align:left"><b>User character:</b> {{userCharacter}}</p>
-    <p style="text-align:left"><b>User gender:</b> {{userGender}}</p>
-    <p style="text-align:left"><b>User email:</b> {{userEmail}}</p>
-    <p style="text-align:left"><b>User phone:</b> {{userPhone}}</p>
-    <p style="text-align:left"><b>studentid:</b> {{studentId}}</p>
-    <p style="text-align:left"><b>teacherid:</b> {{teacherId}}</p>
-    <p style="word-break:break-all;text-align:left"><b>Access token</b>: {{accessToken}}</p>
-    <el-button type="primary" round @click="logout()">退出</el-button>
+      <div class="page">
+        <p>This is the user panel!</p>
+        <p style="text-align:left"><b>User_name:</b> {{user_name}}</p>
+        <p style="text-align:left"><b>User user_id:</b> {{userId}}</p>
+        <p style="text-align:left"><b>User character:</b> {{userCharacter}}</p>
+        <p style="text-align:left"><b>User gender:</b> {{userGender}}</p>
+        <p style="text-align:left"><b>User email:</b> {{userEmail}}</p>
+        <p style="text-align:left"><b>User phone:</b> {{userPhone}}</p>
+        <p style="text-align:left"><b>studentid:</b> {{studentId}}</p>
+        <p style="text-align:left"><b>teacherid:</b> {{teacherId}}</p>
+        <p style="word-break:break-all;text-align:left"><b>Access token</b>: {{accessToken}}</p>
+        <el-button type="primary" round @click="logout()">退出</el-button>
+      </div>
   </div>
 </template>
 
@@ -48,5 +50,7 @@
 </script>
 
 <style scoped>
-
+.page {
+    
+}
 </style>
